6bb7fb529e1701f396ebc3e7e08c10353de84474
hoang Yen PHAM <hoang-yen.pham@etu.u-bordeaux.fr>
2017-04-03 10:24:34 +0200

    updated : lambda 0 & 1 stimulus ok

--- dopamine_cell_RL.py
+++ dopamine_cell_RL.py
@@ -2,8 +2,7 @@
 """Dopamine cell prediction activity"""
 # ==============================================================================
 __author__ = "Anais GRIMAL, Hoang-Yen PHAM"
-__version__ = "1.1"
-__date__ = "20/03/2017"
+__date__ = "03/04/2017"
 # ==============================================================================
 
 from matplotlib import pyplot as plt
@@ -19,21 +18,21 @@ import numpy as np
 # ==============================================================================
 
 T = 5
-N = 10 #time steps occurrence
+N = 25 #time steps occurrence
 h = T/N #time step scale
 gamma = 0.98 #discount factor
 alpha = 0.05 #learning rate
 lamb = 0 #eligibility trace parameter
 
 # === Initialisation ===
-k = 2 #stimuli occurrence >= 1, even for no stimulus
+k = 1 #stimuli occurrence >= 1, even for no stimulus
 x = [[0 for t in range(N)] for i in range(k)] #state vectors of the stimuli, between 0.5 to 2 seconds
 w = [[0 for t in range(N)] for i in range(k)] #weights vector per stimulus
 r = [0 for t in range(N)] #reward
 
 # === Reward prediction ===
 pl = [[x[i][t]*w[i][t] for t in range(N)] for i in range(k)] #reward predictions
-P = pl[0] #total reward prediction
+P = [0 for t in range(N)] #total reward prediction
 if k > 1:
     for t in range(N):
         for i in range(1,k): #begin at 1 <-- P = pl[0]
@@ -61,71 +60,54 @@ delta_w = [[alpha * delta[t] * e[i][t] for t in range(N)] for i in range(k)] #th
 
 for t in range(N):
     for i in range(k):
-        w[i][t] +=  delta_w[i][t] #weights update
-
-# === Learning function ===
-def TD_RL(i, x, r, N, e, pl, P, delta, delta_w, w):
-    q = 0
-    
-    # action
-    for t in range(N):
-        if x[t] == 1:
-            q = t
-        
-        pl[i][t] = x[t] * w[i][t] #reward predictions
-        P[t] += pl[i][t]
-        
-        if t >= 1: # ==0 at t=0, either way it isn't an N-sized list
-            e[i][t] = lamb*e[i][t-1] + x[t-1]
-            TD[t] =  P[t-1] - gamma * P[t] # <0 when predicts a reward at time step t+1
-        
-        delta[t] = r[t] - TD[t]
-        
-        delta_w[i][t] = alpha * delta[t] * e[i][t]
-        w[i][q] += delta_w[i][t]
-    
+        w[i][t] +=  delta_w[i][t] #weights update    
 
 # === Trials ===
-trials = 10
+trials = 100
 for j in range(trials):
     x = [[0 for t in range(N)] for i in range(k)]
     r = [0 for t in range(N)]
 
-    x[0][4] = 1
-    x[1][5] = 1
-    r[6] = 1
-    q = [0 for i in range(k)]
+    s = [5]
+    r[20] = 1
+    q = 0
 
-##    TD_RL(0, x[0], x[1], N, e, pl, P, delta, delta_w, w)
-##    TD_RL(1, x[1], r, N, e, pl, P, delta, delta_w, w)
     # action
     for t in range(N):
         for i in range(k):
-            if x[i][t] == 1:
-                q[i] = t
+            if t >= 1: # ==0 at t=0, either way it isn't an N-sized list
+                e[i] = np.multiply(e[i],lamb)
+                e[i] = np.add(e[i],x[i])
+
+            if t == s[0]:
+                x[0][q] = 1
+                
+            if 1 in x[i] and t > s[0]:
+                try :
+                    x[i][q] = 0
+                    x[i][q+1] = 1
+                    q += 1
+                except: pass
             
-            pl[i][t] = x[i][t] * w[i][t] #reward predictions
-            P[t] += pl[i][t]
+            pl[i] = np.multiply(x[i],w[i]) #reward predictions
+            P[t] = np.sum(pl[i])
             
             if t >= 1: # ==0 at t=0, either way it isn't an N-sized list
-                e[i][t] = lamb*e[i][t-1] + x[i][t-1]
                 TD[t] =  P[t-1] - gamma * P[t] # <0 when predicts a reward at time step t+1
             
             delta[t] = r[t] - TD[t]
-            
-            delta_w[i][t] = alpha * delta[t] * e[i][t]
-            w[i][q[i]] += delta_w[i][t]
-    
-##            if i == 0:
-##                print("eligibilité", e[i][t])
-##                print("delta", delta[t])
-##                print("delta_w",delta_w[i][t])
-##                print("recompense", r[t] == 1)
-##                print("w0", w[i][q[i]])
+            delta_w[i] = np.multiply(alpha * delta[t], e[i])
+            w[i] = np.add(w[i], delta_w[i])
 
-for i in range(N):
+
+
+##print("ce qu'on veut 1:", (alpha**2)*gamma)
+##print("ce qu'on veut 2:", (2*alpha)-(alpha**2))
+
+for i in range(N): #print des valeurs
     if r[i] != 0: print("r", i*h, r[i])
-    if w[0][i] != 0: print("w0", i*h, w[0][i])
+    if w[0][i] != 0:
+        print("w0", i*h, w[0][i])
     if k>1 and w[1][i] != 0: print("w1", i*h, w[1][i])
     if x[0][i] != 0: print("x0", i*h, x[0][i])
     if k>1 and x[1][i] != 0: print("x1", i*h, x[1][i])
@@ -137,13 +119,13 @@ for i in range(N):
 
 axisx = np.arange(0,T,h)
 plt.plot(axisx, delta, 'k', linewidth=4)
+plt.plot(s[0]*h, 1, 'go')
 plt.plot(axisx, r, 'ro')
-plt.plot(axisx, x[0], 'go')
 if k > 1:
-    plt.plot(axisx, x[1], 'go')
+    plt.plot(s[1]*h, 1, 'go')
 # axes limits
 axes = plt.gca()
-axes.set_ylim([0, 3])
+axes.set_ylim([0, 1.5])
 plt.show()
             
             
