a08f8df0124f6a3a14ec8d2691d68e08583b21e5
Unknown <hoang-yen.pham@etu.u-bordeaux.fr>
2017-05-10 16:34:38 +0200

    Updated 3D plots

--- dopamine_cell_RL_3D.py
+++ dopamine_cell_RL_3D.py
@@ -22,18 +22,20 @@ n_trials = {'0.005':800, '0.05':400, '0.5':40, '1':40}
 stimuli = [5, 15]
 cpt = 0
 
-fig = plt.figure(figsize=plt.figaspect(1))
+#fig = plt.figure(figsize=plt.figaspect(1)) #uncomment to subplot
 
 for λ in [1.0, 0.9, 0.6, 0.3, 0.0]:
     for α in [0.005, 0.05, 0.5, 1]:
-        cpt += 1 #for the last subplot parameter
+        cpt += 1
         tdmodel = model.TDModel(λ=λ, α=α, γ=γ, N=N, T=T, stimuli=stimuli)
         for _ in range(n_trials[str(α)]):
             tdmodel.trial()
 
-        #print(tdmodel.δ_history[-1])
-        ax = fig.add_subplot(5, 4, cpt, projection='3d')
-        fig.tight_layout()
+        fig = plt.figure() #comment to subplot
+        ax = fig.gca(projection="3d") #comment to subplot
+        
+        #ax = fig.add_subplot(5, 4, cpt, projection='3d') #uncomment to subplot
+        #fig.tight_layout() #uncomment to subplot
         
         X = np.arange(0, T, T/N)
         Y = np.arange(0, n_trials[str(α)])
@@ -45,9 +47,11 @@ for λ in [1.0, 0.9, 0.6, 0.3, 0.0]:
         ax.set_zlim(-0.1, 1.01)
         ax.zaxis.set_major_locator(LinearLocator(10))
         ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-        #fig.colorbar(surface, shrink=0.5, aspect=5)
-        plt.axis("off")
-        #plt.savefig('figures/dopamine_cell_RL_3D%s_%s.png' % (lamb, α))
 
-plt.show()
-plt.close()
+        #fig.colorbar(surface, shrink=0.5, aspect=5)
+        #plt.axis("off") #uncomment to subplot
+
+        plt.savefig('figures/dopamine_cell_RL_3D%s.png' % (cpt)) #comment to subplot
+        #plt.show()
+        
+#plt.close()
